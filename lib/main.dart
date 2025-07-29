68efbb1cdf79b1e1ee4b90562b5b5c814efcfcc3
Zzeolla <shincw1142@gmail.com>
2025-07-29 20:53:54 +0900

    before first release

--- lib/main.dart
+++ lib/main.dart
@@ -100,7 +100,7 @@ class MyApp extends StatelessWidget {
         fontFamily: 'NotoSansKR',
 
         // 카드 테마
-        cardTheme: CardTheme(
+        cardTheme: CardThemeData(
           elevation: 2,
           shape: RoundedRectangleBorder(
             borderRadius: BorderRadius.circular(12),
