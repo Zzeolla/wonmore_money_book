1b26f0a1ef55cc95187035d2da90fe3774fab1b4
Zzeolla <shincw1142@gmail.com>
2025-08-09 20:24:29 +0900

    change ads id for test code

---
+++ lib/util/ad_config.dart
@@ -0,0 +1,3 @@
+class AdConfig {
+  static const bool useTestAds = bool.fromEnvironment('USE_TEST_ADS', defaultValue: true);
+}
--- lib/util/ad_helper.dart
+++ lib/util/ad_helper.dart
@@ -1,42 +1,40 @@
 // lib/util/ad_helper.dart
 import 'dart:io';
 import 'package:flutter/foundation.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart'; // for kDebugMode
-
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:wonmore_money_book/util/ad_config.dart'; // for kDebugMode
+/// TODO : 출시 전에는 반드시 옵션으로 false 지정 필요 flutter build appbundle --dart-define=USE_TEST_ADS=false
 class AdHelper {
   static String get bannerAdUnitId {
-    if (kDebugMode) {
+    if (AdConfig.useTestAds) {
       return Platform.isAndroid
           ? 'ca-app-pub-3940256099942544/6300978111' // ✅ 테스트용
           : 'ca-app-pub-3940256099942544/2934735716';
-    } else {
-      return Platform.isAndroid
-          ? dotenv.env['ADMOB_BANNER_ID_ANDROID']!
-          : dotenv.env['ADMOB_BANNER_ID_IOS']!;
     }
+    return Platform.isAndroid
+        ? dotenv.env['ADMOB_BANNER_ID_ANDROID']!
+        : dotenv.env['ADMOB_BANNER_ID_IOS']!;
   }
 
   static String get rewardedInterstitialAdUnitId {
-    if (kDebugMode) {
+    if (AdConfig.useTestAds) {
       return Platform.isAndroid
           ? 'ca-app-pub-3940256099942544/5354046379' // ✅ 테스트용
           : 'ca-app-pub-3940256099942544/6978759866';
-    } else {
-      return Platform.isAndroid
-          ? dotenv.env['ADMOB_REWARDED_ID_ANDROID']!
-          : dotenv.env['ADMOB_REWARDED_ID_IOS']!;
     }
+    return Platform.isAndroid
+        ? dotenv.env['ADMOB_REWARDED_ID_ANDROID']!
+        : dotenv.env['ADMOB_REWARDED_ID_IOS']!;
   }
 
   static String get interstitialAdUnitId {
-    if (kDebugMode) {
+    if (AdConfig.useTestAds) {
       return Platform.isAndroid
           ? 'ca-app-pub-3940256099942544/1033173712' // ✅ 테스트용
           : 'ca-app-pub-3940256099942544/4411468910';
-    } else {
-      return Platform.isAndroid
-          ? dotenv.env['ADMOB_INTERSTITIAL_ID_ANDROID']!
-          : dotenv.env['ADMOB_INTERSTITIAL_ID_IOS']!;
     }
+    return Platform.isAndroid
+        ? dotenv.env['ADMOB_INTERSTITIAL_ID_ANDROID']!
+        : dotenv.env['ADMOB_INTERSTITIAL_ID_IOS']!;
   }
 }
